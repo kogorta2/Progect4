1ae06f46d19321c18b5e5b0b6477ed76006588cc
kogorta2 <kogorta2@gmail.com>
2017-04-08 00:29:27 +0300

    +footer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,87 +2,28 @@ $(document).foundation();
 
 $(document).ready(function () {
 
-    // var handleMatchMedia = function (mediaQuery) {
-    //         var $ButtonEvent = $('\
-    //                <div class="wrapper-for-button">\
-	 //                    <div class="row wrapper">\
-	 //                        <div class="small-4 medium-2 columns button-for-menu">\
-		// 					    <i class="fa fa-menu" aria-hidden="true"></i>\
-	 //                        </div>\
-		// 				</div>\
-		// 			  </div>');
-    //
-    //         var $menuLarge = $('\
-    //    <div class="menu">\
-    //     <div class="row  align-middle">\
-    //         <ul class="small-9 medium-9 large-8 columns">\
-    //             <li><a href="#">Home</a></li>\
-    //             <li><a href="#">About us</a><div class="li-border"></div></li>\
-    //             <li><a href="#">services</a><div class="li-border"></div></li>\
-    //             <li><a href="#">news</a><div class="li-border"></div></li>\
-    //             <li><a href="#">blog</a><div class="li-border"></div></li>\
-    //             <li><a href="#">pages</a><div class="li-border"></div></li>\
-    //             <li><a href="#">Contact Us</a><div class="li-border"></div></li>\
-    //         </ul>\
-    //     </div>\
-    // </div>');
-    //
-    //         var $menuUnderButton = $('\
-    //     <div class="wrapper-background">\
-		//                      <div class="row align-middle">\
-		// 						<div class="small-4 medium-2 columns">\
-		// 							<ul>\
-		// 								<li><a href="#">Home</a></li>\
-		// 								<li><a href="#">About us</a></li>\
-		// 								<li><a href="#">services</a></li>\
-		// 								<li><a href="#">news</a></li>\
-		// 								<li><a href="#">blog</a></li>\
-		// 								<li><a href="#">pages</a></li>\
-		// 								<li><a href="#">Contact Us</a></li>\
-		// 							</ul>\
-		// 						</div>\
-		// 					</div>\
-		// 				</div>');
-    //
-    //     if (mediaQuery.matches) {
-    //
-    //         $('.menu').replaceWith($ButtonEvent);
-    //         $($(".wrapper").children()).on('click', function (e) {
-    //             if ($(".header").hasClass("view-menu")) {
-    //                 $('.wrapper-background').detach();
-    //                 $(".header").removeClass("view-menu");
-    //             }
-    //             else {
-    //                 $($menuUnderButton).insertBefore($('.wrapper-for-button'));
-    //                 $(".header").addClass("view-menu");
-    //             }
-    //             return false;
-    //         });
-    //     }
-    //     else {
-    //         if ($(".header").hasClass("view-menu")) {
-    //             $('.wrapper-background').detach();
-    //             $(".header").removeClass("view-menu");
-    //         }
-    //         $('.wrapper-for-button').replaceWith($menuLarge);
-    //
-    //     }
-    //         return false;
-    // },
-    //     wMm=window.matchMedia('all and (max-width:1024px)');
-    // handleMatchMedia(wMm);
-    // wMm.addListener(handleMatchMedia);
+    $('.clients-slider').slick({
+        arrows:false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+
+    });
+
 
     
     
     
-    
-    
-    $('.burger-button').on('click',function (e) {
+    $('.burger-button.up').on('click',function (e) {
         $('.menu').find('ul').toggleClass('click-on');
         $('.menu').find('ul').toggleClass('click-off');
     })
 
+    $('.burger-button.down').on('click',function (e) {
+        $('.footer-menu').find('ul').toggleClass('click-on');
+        $('.footer-menu').find('ul').toggleClass('click-off');
+    })
 
 })
 
